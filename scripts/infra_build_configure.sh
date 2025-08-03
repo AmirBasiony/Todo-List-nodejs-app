04a37eada2a0bc611081272244af17e98d998cec
AmirBasiony <amirbasiony14@gmail.com>
2025-08-04 01:44:00 +0300

    Auto: Infrastructure deployment

--- scripts/infra_build_configure.sh
+++ scripts/infra_build_configure.sh
@@ -17,18 +17,18 @@ cd "$TERRAFORM_DIR" || exit 1
 
 # Build infrastructure
 section_header "*********************    Building the infrastructure     **********************"
-# terraform init #-reconfigure
-# terraform validate
-# terraform apply -auto-approve #-refresh=false 
+terraform init #-reconfigure
+terraform validate
+terraform apply -auto-approve #-refresh=false 
 
-# if [ $? -ne 0 ]; then
-#   echo "ERROR: Terraform apply failed."
-#   echo "Please check the Terraform configuration and try again."
-#   exit 1
-# fi
+if [ $? -ne 0 ]; then
+  echo "ERROR: Terraform apply failed."
+  echo "Please check the Terraform configuration and try again."
+  exit 1
+fi
 
-# mkdir -p "$INFRA_DIAGRAM_DIR"
-# terraform graph | dot -Tpng > "$INFRA_DIAGRAM_DIR/[Terraform]_Infra_In-Depth.png"
+mkdir -p "$INFRA_DIAGRAM_DIR"
+terraform graph | dot -Tpng > "$INFRA_DIAGRAM_DIR/[Terraform]_Infra_In-Depth.png"
 
 # Fetch values from Terraform outputs
 PRIVATE_ID_1=$(terraform output -raw web_server_private1_id)
@@ -37,19 +37,20 @@ SSM_BUCKET=$(terraform output -raw ansible_ssm_bucket_name)
 AWS_REGION=$(terraform output -raw aws_region)
 APP_URL=$(terraform output -raw alb_dns_name)
 SSM_TIMEOUT="60"
-# Display key info
-# section_header "*******************   Infrastructure Deployed Successfully   ******************"
-# echo           "*****************           Private EC2 Instance IDs           ****************"
-# echo           "******************            $PRIVATE_ID_1            ******************"
-# echo           "******************            $PRIVATE_ID_2            ******************"
-# echo           "********   APP URL: $APP_URL    *********"
-# echo           "************************     AWS Region: $AWS_REGION     ************************"
-# echo           "*******************************************************************************"
 
-# if [[ -z "$PRIVATE_ID_1" || -z "$PRIVATE_ID_2" || -z "$AWS_REGION" ]]; then
-#   echo "ERROR: One or more required Terraform outputs are missing."
-#   exit 1
-# fi
+# Display key info
+section_header "*******************   Infrastructure Deployed Successfully   ******************"
+echo           "*****************           Private EC2 Instance IDs           ****************"
+echo           "******************            $PRIVATE_ID_1            ******************"
+echo           "******************            $PRIVATE_ID_2            ******************"
+echo           "********   APP URL: $APP_URL    *********"
+echo           "************************     AWS Region: $AWS_REGION     ************************"
+echo           "*******************************************************************************"
+
+if [[ -z "$PRIVATE_ID_1" || -z "$PRIVATE_ID_2" || -z "$AWS_REGION" ]]; then
+  echo "ERROR: One or more required Terraform outputs are missing."
+  exit 1
+fi
 
 # Navigate to Ansible directory
 cd "$ANSIBLE_DIR" || exit 1
